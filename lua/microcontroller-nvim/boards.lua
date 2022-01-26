c7da4203c0b0acf4aa63f631d33fd64f4407a76b
Mads Kjeldgaard <mail@madskjeldgaard.dk>
2022-01-26 13:08:51 +0100

    format

--- lua/microcontroller-nvim/boards.lua
+++ lua/microcontroller-nvim/boards.lua
@@ -24,36 +24,36 @@ end
 
 -- Fuzzy search platformio boards and add the selected to platformio.ini if it exists
 function M.boards()
-coroutine.wrap(function()
-	local board_info = pio_boards()
-  local result = fzf.fzf(utils.table_keys(board_info))
+	coroutine.wrap(function()
+		local board_info = pio_boards()
+		local result = fzf.fzf(utils.table_keys(board_info))
 
-  if result then
-	  local key = result[1]
-	  local info = board_info[key]
-	  local out_string = string.format([[
+		if result then
+			local key = result[1]
+			local info = board_info[key]
+			local out_string = string.format([[
 
 [env:%s]
 platform = %s
 framework = %s
 board = %s
 ]],
-	info.id,
-	info.platform,
-	info.frameworks[1], -- For now, just choose the first framework
-	info.id
-)
+				info.id,
+				info.platform,
+				info.frameworks[1], -- For now, just choose the first framework
+				info.id
+			)
 
-		-- TODO This file checking doesn't seem to work?
-		local out_file = "platformio.ini"
-		local file_exists, _ = utils.exists(out_file)
-		if file_exists then
-			utils.append_to_file(out_string, out_file)
-		else
-			error(string.format("Could not find %s", out_file))
+			-- TODO This file checking doesn't seem to work?
+			local out_file = "platformio.ini"
+			local file_exists, _ = utils.exists(out_file)
+			if file_exists then
+				utils.append_to_file(out_string, out_file)
+			else
+				error(string.format("Could not find %s", out_file))
+			end
 		end
-  end
-end)()
+	end)()
 end
 
 return M
