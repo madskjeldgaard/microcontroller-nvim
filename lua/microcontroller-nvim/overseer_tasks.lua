8c9c3373b4a2651d2ea50d182c57c568bfc13b7b
Mads Kjeldgaard <mail@madskjeldgaard.dk>
2022-08-08 18:51:20 +0200

    cleanup

--- lua/microcontroller-nvim/overseer_tasks.lua
+++ lua/microcontroller-nvim/overseer_tasks.lua
@@ -1,7 +1,7 @@
 local status, overseer = pcall(require, 'overseer')
 
 --- Check if a file or directory exists in this path
-function exists(file)
+local function exists(file)
    local ok, err, code = os.rename(file, file)
    if not ok then
       if code == 13 then
@@ -13,7 +13,7 @@ function exists(file)
 end
 
 --- Check if a directory exists in this path
-function isdir(path)
+local function isdir(path)
    -- "/" works on both Unix and Windows
    return exists(path.."/")
 end
@@ -21,142 +21,82 @@ end
 if status then
 	-- Build
 	overseer.register_template({
-		-- Required fields
 		name = "piobuild",
 		builder = function(params)
-			-- This must return an overseer.TaskDefinition
 			return {
-				-- cmd is the only required field
 				cmd = {'pio'},
-				-- additional arguments for the cmd
-				args = {"run"},
-				-- the name of the task (defaults to the cmd of the task)
+				args = {"run", "--verbose"},
 				name = "piobuild",
-				-- set the working directory for the task
 				cwd = "",
-				-- additional environment variables
-				env = {
-					-- VAR = "FOO",
+				env = {},
+				components = {
+					"default",
+					"on_result_diagnostics",
+					["on_result_diagnostics_quickfix"] = {
+						open = true
+					}
 				},
-				-- the list of components or component aliases to add to the task
-				components = {"default"},
-				-- arbitrary table of data for your own personal use
-				metadata = {
-					-- foo = "bar",
-				},
+				metadata = {},
 			}
 		end,
-		-- Optional fields
 		desc = "Build a platformio project",
-		-- Tags can be used in overseer.run_template()
 		tags = {overseer.TAG.BUILD},
-		params = {
-			-- See :help overseer.params
-		},
-		-- Determines sort order when choosing tasks. Lower comes first.
+		params = { },
 		priority = 50,
-		-- Add requirements for this template. If they are not met, the template will not be visible.
-		-- All fields are optional.
 		condition = {
-			-- A string or list of strings
-			-- Only matches when current buffer is one of the listed filetypes
 			filetype = {"c", "cpp", "ino", "h", "hpp", "ini"},
-				-- Only activate if it is in a platformio directory
 			callback = function(search)
 				return isdir(search.dir .. "/.pio")
-			end,
+			end
 		},
 	})
 
 	-- Upload
 	overseer.register_template({
-		-- Required fields
 		name = "pioupload",
 		builder = function(params)
-			-- This must return an overseer.TaskDefinition
 			return {
-				-- cmd is the only required field
 				cmd = {'pio'},
-				-- additional arguments for the cmd
-				args = {"run", "-t", "upload"},
-				-- the name of the task (defaults to the cmd of the task)
+				args = {"run", "-t", "upload", "--verbose"},
 				name = "pioupload",
-				-- set the working directory for the task
 				cwd = "",
-				-- additional environment variables
-				env = {
-					-- VAR = "FOO",
-				},
-				-- the list of components or component aliases to add to the task
+				env = { },
 				components = {"default"},
-				-- arbitrary table of data for your own personal use
-				metadata = {
-					-- foo = "bar",
-				},
+				metadata = { },
 			}
 		end,
-		-- Optional fields
 		desc = "Build and upload a platformio project to a microcontroller",
-		-- Tags can be used in overseer.run_template()
 		tags = {overseer.TAG.BUILD},
-		params = {
-			-- See :help overseer.params
-		},
-		-- Determines sort order when choosing tasks. Lower comes first.
+		params = { },
 		priority = 50,
-		-- Add requirements for this template. If they are not met, the template will not be visible.
-		-- All fields are optional.
 		condition = {
-			-- A string or list of strings
-			-- Only matches when current buffer is one of the listed filetypes
 			filetype = {"c", "cpp", "ino", "h", "hpp", "ini"},
 			callback = function(search)
 				return isdir(search.dir .. "/.pio")
-			end,
-		},
+			end
+		}
 	})
 
 	-- Check
 	overseer.register_template({
-		-- Required fields
 		name = "piocheck",
 		builder = function(params)
-			-- This must return an overseer.TaskDefinition
 			return {
-				-- cmd is the only required field
 				cmd = {'pio'},
-				-- additional arguments for the cmd
 				args = {"check", "--verbose"},
-				-- the name of the task (defaults to the cmd of the task)
 				name = "piocheck",
-				-- set the working directory for the task
 				cwd = "",
-				-- additional environment variables
-				env = {
-					-- VAR = "FOO",
-				},
-				-- the list of components or component aliases to add to the task
+				env = {},
 				components = {"default"},
-				-- arbitrary table of data for your own personal use
-				metadata = {
-					-- foo = "bar",
-				},
+				metadata = {},
 			}
 		end,
-		-- Optional fields
 		desc = "Run static checker on a platformio project",
-		-- Tags can be used in overseer.run_template()
 		tags = {overseer.TAG.BUILD},
 		params = {
-			-- See :help overseer.params
 		},
-		-- Determines sort order when choosing tasks. Lower comes first.
 		priority = 50,
-		-- Add requirements for this template. If they are not met, the template will not be visible.
-		-- All fields are optional.
 		condition = {
-			-- A string or list of strings
-			-- Only matches when current buffer is one of the listed filetypes
 			filetype = {"c", "cpp", "ino", "h", "hpp", "ini"},
 			callback = function(search)
 				return isdir(search.dir .. "/.pio")
